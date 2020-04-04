c98a666a2e82d41f920a1a1c68d24509a1f527f6
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-04-04 17:26:45 -0500

    Corrección de argumento FindByIdAndUpdate y modificación del formato del archivo

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,27 +1,26 @@
 const userModel = require("../models/userModel");
 
-exports.getUsers = async user => {
-//  if (user.query.username) {
-//    return await userModel.findOne({ username: userModel.query.username });
-//  }
+exports.getUsers = async (user) => {
+  //  if (user.query.username) {
+  //    return await userModel.findOne({ username: userModel.query.username });
+  //  }
   return await userModel.find();
 };
 
-exports.createUser = async idUser => {
+exports.createUser = async (idUser) => {
   return await userModel.create(idUser);
 };
 
-exports.deleteUser = async idUser => {
+exports.deleteUser = async (idUser) => {
   return await userModel.findByIdAndDelete(idUser);
 };
 
-exports.findUser = async idUser => {
-  return await userModel
-  .findById(idUser)
+exports.findUser = async (idUser) => {
+  return await userModel.findById(idUser);
 };
 
 exports.updateUser = async (idUser, data) => {
-  return await userModel.findByIdUpdate(idUser, data, {
-    new: true
+  return await userModel.findByIdAndUpdate(idUser, data, {
+    new: true,
   });
 };
