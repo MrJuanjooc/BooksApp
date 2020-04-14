47ce3c90cd661bebcb5d57f828fe36c36e83462b
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-04-14 18:17:25 -0500

    Se crea el middleware para authenication

---
+++ app/middlewares/authMiddleware.js
@@ -0,0 +1,19 @@
+const jwt = require("jsonwebtoken"),
+  config = require("../configs/config"),
+  authException = require("../exceptions/notAuthException");
+
+module.exports = (req, res, next) => {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    throw new authException();
+  }
+
+  jwt.verify(token, config.SECRET, (err, decToken) => {
+    if (err) {
+      throw new authException();
+    }
+    req.user = decToken.user;
+    next();
+  });
+};
