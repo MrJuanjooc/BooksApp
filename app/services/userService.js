8a219f878f03241b3f74a0acbff0cf69fb64f933
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-04-15 18:48:26 -0500

    Se agrego createUser y se añadio en las funciones getUsers y findUser el populate para tomar la información del libreo asociado al usuario

--- app/services/userService.js
+++ app/services/userService.js
@@ -1,22 +1,30 @@
 const userModel = require("../models/userModel");
 
 exports.getUsers = async (user) => {
-  //  if (user.query.username) {
-  //    return await userModel.findOne({ username: userModel.query.username });
-  //  }
+  if (user.query.username) {
+    return await userModel
+      .findOne({ username: userModel.query.username })
+      .populate("Book", "nombre", "descripcion", "autor", "imagen");
+  }
   return await userModel.find();
 };
 
-exports.deleteUser = async (idUser) => {
-  return await userModel.findByIdAndDelete(idUser);
-};
-
 exports.findUser = async (idUser) => {
-  return await userModel.findById(idUser);
+  return await userModel
+    .findById(idUser)
+    .populate("Book", "nombre", "descripcion", "autor", "imagen");
 };
 
 exports.updateUser = async (idUser, data) => {
   return await userModel.findByIdAndUpdate(idUser, data, {
     new: true,
   });
+};
+
+exports.deleteUser = async (idUser) => {
+  return await userModel.findByIdAndDelete(idUser);
+};
+
+exports.createUser = async (user) => {
+  return await userModel.create(user);
 };
