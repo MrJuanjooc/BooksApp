a6576bb3f10b4afe6599036e37fbcd6c44495adb
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-04-14 21:43:43 -0500

    cambio en el Schema, se agrega los campos password y favoritos, adicional los métodos toJSON, comparePassword y cifrado del password

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -1,4 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require("mongoose"),
+  Schema = mongoose.Schema,
+  bcrypt = require("bcryptjs");
 
 let userScheme = new mongoose.Schema({
   username: {
@@ -9,6 +11,42 @@ let userScheme = new mongoose.Schema({
     type: String,
     require: true,
   },
+  password: {
+    type: String,
+    require: true,
+  },
+  favoritos: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Book",
+    },
+  ],
+});
+
+userScheme.methods.toJSON = function () {
+  //trae los objetos de mongoose y los convierte a .json
+  let user = this.toObject();
+  delete user.password;
+  return user;
+};
+
+//dispara un trigger antes de guardar
+userScheme.methods.comparePassword = async function (password) {
+  let result = await bcrypt.compare(password, this.password);
+  return result;
+};
+
+//dispara un trigger antes de guardar 
+userScheme.pre('save', async function(next){
+  const user = this;
+  if (!user.isModified('password')) {
+    return next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(user.password, salt); //Cifrar el password
+  user.password = hashedPassword;
+  return next();
 });
 
 mongoose.model("User", userScheme);
