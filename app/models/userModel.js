e20e920e4a76f85b5b16acafd44a28b0e5d18a0d
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-04-18 07:25:28 -0500

    Se retiraron obligatoriedad de los campos del schema de User

--- app/models/userModel.js
+++ app/models/userModel.js
@@ -5,15 +5,12 @@ const mongoose = require("mongoose"),
 let userScheme = new mongoose.Schema({
   username: {
     type: String,
-    require: true,
   },
   name: {
     type: String,
-    require: true,
   },
   password: {
     type: String,
-    require: true,
   },
   favoritos: [
     {
