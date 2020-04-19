816ae1764c8b540b35af0ca25708b7c5407e7a87
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-04-18 23:45:37 -0500

    se agregaron comentarios con los libros para usarlos en las pruebas

--- app/models/bookModel.js
+++ app/models/bookModel.js
@@ -23,3 +23,28 @@ let bookScheme = new mongoose.Schema({
 
 mongoose.model("Book", bookScheme);
 module.exports = mongoose.model("Book");
+/*
+{
+	"nombre": 		"1984",
+	"descripcion":	"En el año 1984 Londres es una ciudad lúgrube en la que la Policia del pensamiento...",
+	"autor":	"George Orwell",
+	"imagen": "1236",
+	"categoria":	["Guerra, Política, Romance"]
+}
+
+{
+	"nombre": 		"La culpa es de la Vaca",
+	"descripcion":	"Libro de Enseñanza",
+	"autor":	"Jaime Lopera Gutierrez y Marta Inés Bernal Trujillo",
+	"imagen": "aaaaa",
+	"categorias": ["Anécdotas, parábolas, fábulas y reflexiones sobre el liderazgo"]
+}
+
+{
+	"nombre": 		"La semilla del Diablo",
+	"descripcion":	"Libro de Terror y Suspenso",
+	"autor":	"Ira Levin",
+	"imagen": "1236",
+	"categorias":	["Terror, Drama, Suspenso"]
+}
+*/
