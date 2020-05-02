c450e5c020a77f1f8c237f8c93b2cb7cbc0553a1
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-05-02 17:40:40 -0500

    Cambio de los mensajes de errores para controlarlos con reqExceptions

--- app/controllers/bookController.js
+++ app/controllers/bookController.js
@@ -1,4 +1,6 @@
-const bookService = require("../services/bookService");
+const bookService = require("../services/bookService"),
+  reqExceptions = require("../exceptions/reqExceptions"),
+  authMiddleware = require("../middlewares/authMiddleware");
 
 exports.getBooks = async (req, res) => {
   try {
@@ -6,9 +8,10 @@ exports.getBooks = async (req, res) => {
     getResult = await bookService.getBooks(value);
 
     if (getResult == "") {
-      res
-        .status(404)
-        .send({ error: "El parametro ingresado no coincide con ningún libro" });
+      throw new reqExceptions(
+        "No exite un libro con los criterios ingresados",
+        401
+      );
     }
     res.status(200).send(getResult);
   } catch (err) {
@@ -20,6 +23,9 @@ exports.createBook = async (req, res) => {
   try {
     const idBook = req.body;
     postResult = await bookService.createBook(idBook);
+    if (!postResult) {
+      throw new reqExceptions("Error al crear el Libro", 400);
+    }
     res.status(200).send(postResult);
   } catch (err) {
     res.status(500).send(err);
@@ -28,13 +34,11 @@ exports.createBook = async (req, res) => {
 
 exports.deleteBook = async (req, res) => {
   try {
-    const idBook = req.params.id;
+    const idBook = req.params.libroId;
     deleteResult = await bookService.deleteBook(idBook);
 
     if (!deleteResult) {
-      res
-        .status(404)
-        .send({ error: "Error al elminar el Libro, este no existe" });
+      throw new reqExceptions("Error al elminar el Libro, este no existe", 400);
     }
     res.status(200).send(deleteResult);
   } catch (err) {
@@ -44,11 +48,11 @@ exports.deleteBook = async (req, res) => {
 
 exports.findBook = async (req, res) => {
   try {
-    const idBook = req.params.id;
+    const idBook = req.params.libroId;
     findResult = await bookService.findBook(idBook);
 
     if (!findResult) {
-      res.status(404).send({ error: "El libro no existe" });
+      throw new reqExceptions("El libro no existe", 400);
     }
     res.status(200).send(findResult);
   } catch (err) {
@@ -57,18 +61,54 @@ exports.findBook = async (req, res) => {
 };
 
 exports.updateBook = async (req, res) => {
-  const idBook = req.params.id;
+  const idBook = req.params.libroId;
   const data = req.body;
 
   try {
     let putResult = await bookService.updateBook(idBook, data);
 
     if (!putResult) {
-      res
-        .status(404)
-        .send({ error: "No Se Actulizo el Libro debido a que no existe" });
+      throw new reqExceptions(
+        "No Se Actulizo el Libro debido a que no existe",
+        400
+      );
     }
     res.status(200).send(putResult);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.addfavorite = async (req, res) => {
+  const idBook = req.params.libroId,
+    idUser = req.user;
+
+  console.log(idBook);
+  console.log(idUser);
+
+  try {
+    let addFavorite = await bookService.addFavorite(idBook, idUser);
+
+    if (!addFavorite) {
+      throw reqExceptions("Error al agregar el libro a Favoritos", 400);
+    }
+    res.status(200).send(addFavorite);
+  } catch (err) {
+    res.status(500).send(err);
+  }
+};
+
+exports.rmfavorite = async (req, res) => {
+  const idBook = req.params.libroId,
+    idUser = req.user;
+
+  try {
+    let rmFavorite = await bookService.rmFavorite(idBook, idUser);
+
+    if (!rmFavorite) {
+      throw reqExceptions("Error al eliminar el libro de Favoritos", 400);
+    }
+    res.status(200).send(rmFavorite);
   } catch (err) {
     res.status(500).send(err);
   }
