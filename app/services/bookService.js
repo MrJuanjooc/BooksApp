6a6dc7a3b7f2110fc587a1c5964959ede95fe996
mrjuanjooc <juanjoseocampo95@gmail.com>
2020-05-02 17:22:10 -0500

    Se añade las funcionas addFavorite y rmFavorite al bookService

--- app/services/bookService.js
+++ app/services/bookService.js
@@ -1,4 +1,5 @@
-const bookModel = require("../models/bookModel");
+const bookModel = require("../models/bookModel"),
+  userModel = require("../models/userModel");
 
 exports.getBooks = async (value) => {
   let books = await bookModel.find({
@@ -26,5 +27,29 @@ exports.findBook = async (idBook) => {
 exports.updateBook = async (idBook, data) => {
   return await bookModel.findByIdAndUpdate(idBook, data, {
     new: true,
+  });
+};
+
+exports.addFavorite = async (idBook, idUser) => {
+  let addResult = await userModel.update(
+    { _id: idUser },
+    { $addToSet: { favoritos: idBook } }
+  );
+
+  return await userModel.findById(idUser).populate({
+    path: "favoritos",
+    select: "nombre descripcion autor imagen categorias",
+  });
+};
+
+exports.rmFavorite = async (idBook, idUser) => {
+  let rmResult = await userModel.update(
+    { _id: idUser },
+    { $pull: { favoritos: idBook } }
+  );
+
+  return await userModel.findById(idUser).populate({
+    path: "favoritos",
+    select: "nombre descripcion autor imagen categorias",
   });
 };
